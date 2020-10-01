7ee739ca45a191951747f22b6f07ec0c9fdac1f5
Hassan Mian <mian@willandskill.se>
2020-10-01 13:43:51 +0200

    Add firebaseConfig

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import * as firebase from "firebase/app"
+import "firebase/database"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDE_X6vkFlUKKqdHkLPOTLkGZ4PTFPIsiA",
+  authDomain: "js-lessons-a0fc7.firebaseapp.com",
+  databaseURL: "https://js-lessons-a0fc7.firebaseio.com",
+  projectId: "js-lessons-a0fc7",
+  storageBucket: "js-lessons-a0fc7.appspot.com",
+  messagingSenderId: "847970990199",
+  appId: "1:847970990199:web:e451a463fd5b72ad3fae72",
+  measurementId: "G-EC7MRCPFN5"
+};
